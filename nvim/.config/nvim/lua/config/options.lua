940b77c02aeaf27aeb28c051e3376aa3618c3d7b
Rusty Phillips <walter.phillips@ext.airbnb.com>
2025-09-23 18:41:35 -0400

    Switched to LazyVIM.

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,21 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Set leader key to "."
+vim.g.mapleader = "."
+vim.g.maplocalleader = "."
+
+-- System clipboard integration
+vim.opt.clipboard = "unnamedplus"
+
+-- Auto change directory to project root
+vim.opt.autochdir = false -- Don't auto change to file directory
+vim.opt.autowrite = true -- Auto save when switching buffers
+
+-- Configure LazyVim root detection (prioritize Gradle over git)
+vim.g.root_spec = {
+  { "build.gradle.kts", "build.gradle", "pom.xml" },
+  { ".git", "lua" },
+  "cwd"
+}
